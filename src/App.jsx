9b2a306fd3d8b2d0bf7e6cf8492aad8882803ba2
shubham sharma <shubhamtech004@gmail.com>
2025-04-14 18:53:58 +0530

    navbar and footer layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-
+import React from 'react'
 
 import './App.css'
+import Footer from './Components/Footer'
+import Navbar from './Components/Navbar'
 
-function App() {
- 
-
+const App = () =>{
   return (
-   <>
-    <h1>avni-website-dmp2025</h1>
-   </>
-  )
+    <>
+     <Navbar/>
+     <h1 class="text-3xl font-bold underline">
+     Hello world!
+   </h1>
+     <Footer/>
+    
+    </>
+  ) 
 }
 
 export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <>
+            <div className="flex justify-around p-5">
+                <div>logo</div>
+                <div>content</div>
+                <div> button</div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
