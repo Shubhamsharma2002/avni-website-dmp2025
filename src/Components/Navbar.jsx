92e2a2be530a55c2db8388a4d6c854f3e504e132
shubham sharma <shubhamtech004@gmail.com>
2025-04-16 01:46:47 +0530

    nav design done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-
+import { FaBars } from "react-icons/fa";
 const Navbar = () =>{
-    return(
+     return(
         <>
-            <div className="flex justify-around p-5">
-                <div>logo</div>
-                <div>content</div>
-                <div> button</div>
-            </div>
+              <div className="flex justify-around text-black py-6 px-8  md:px-32 bg-white drop-shadow-md">
+                       <div>log</div>
+                       <div className="hidden xl:flex gap-12">
+                          <p>hhh</p>
+                          <p>hhh</p>
+                          <p>hhh</p>
+                          <p>hhh</p>
+                       </div>
+                       <div className=" hidden xl:flex  gap-12">
+                          <p>bb</p>
+                          <p>bb</p>
+                       </div>
+
+                       <div className="block xl:hidden cursor-pointer">
+                       <FaBars />
+                       </div>
+              </div>
         </>
-    )
+     )
 }
 
 export default Navbar;
