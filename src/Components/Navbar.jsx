cd41749f4a6f9f8a5300874854992ab6bdcfd5b9
shubham sharma <shubhamtech004@gmail.com>
2025-04-16 01:58:04 +0530

    responsive navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 const Navbar = () =>{
+    const [menu,setMenu] = useState(false)
      return(
         <>
               <div className="flex justify-around text-black py-6 px-8  md:px-32 bg-white drop-shadow-md">
@@ -17,8 +18,17 @@ const Navbar = () =>{
                        </div>
 
                        <div className="block xl:hidden cursor-pointer">
-                       <FaBars />
+                       <FaBars  onClick={() => setMenu(!menu)}/>
                        </div>
+                      
+                       <div className={`flex flex-col absolute items-center top-full left-0 w-full bg-white p-6 gap-6 xl:hidden transition-all duration-300 ease-in-out ${menu ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4  pointer-events-none' }`}>
+                            <p>hh</p>
+                            <p>hhh</p>
+                            <p>hhhh</p>
+                           </div>
+                       
+       
+    
               </div>
         </>
      )
