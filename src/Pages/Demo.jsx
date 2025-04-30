e24a7ac13a14379f529e790579bb642c25a5ed39
shubham sharma <shubhamtech004@gmail.com>
2025-04-30 21:14:01 +0530

    working on demo page

--- src/Pages/Demo.jsx
+++ src/Pages/Demo.jsx
@@ -1,10 +1,12 @@
 
 import React from "react";
+import ContactForm from "../Components/ContactForm";
 
 const Demo = () =>{
     return(
         <>
           <div className="text-center">wirking on demo  pahe</div>
+          <ContactForm/>
         </>
     )
 }
