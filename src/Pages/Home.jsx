28c038b2a5c988cd06071e17c1a3d40f8cf9ad0e
shubham sharma <shubhamtech004@gmail.com>
2025-05-05 17:48:06 +0530

    info section done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,6 +27,28 @@ const Homepage = () =>{
            <Card img={patform} heading="Platform training" para="Get trained to do full customisation yourself."/>
            <Card img={support} heading="Technical support" para="Annual maintenance support for your customisations and integrations."/>
          </div>
+         <div className="flex flex-col items-center max-w-5xl mx-auto bg-amber-200 mt-10 p-6">
+  <h1 className="text-center font-bold text-xl sm:text-2xl md:text-3xl lg:text-3xl mt-6 mb-4">
+    In-house data collection applications are unsustainable
+  </h1>
+
+  <p className="text-center px-4 mb-4 max-w-3xl">
+    While development of in-house data collection applications may seem feasible and even exciting in the beginning, they become difficult to sustain for most NGOs. We have come across these situations many times, and that is partly the reason we developed Avni.
+  </p>
+
+  <p className="text-center px-4 mb-4 max-w-3xl">
+    Organisations’ requirements both increase and evolve significantly over time. This demands constant improvements to the software, and hence regular funding. It is difficult to retain software developers after the initial development phase, so after early success, NGOs often struggle.
+  </p>
+
+  <p className="text-center px-4 mb-4 max-w-3xl">
+    Avni has effectively utilised extremely scarce philanthropic resources to create a shared software platform. As a result, it offers a comprehensive tool to all non-profit organisations. As this platform evolves—with added or improved features—it becomes available for free to organisations already using it.
+  </p>
+
+  <h1 className="text-center font-bold text-xl sm:text-2xl mt-6">
+    Avni makes your digital transformation sustainable
+  </h1>
+</div>
+
         <ContactForm/> 
          </>
     )
